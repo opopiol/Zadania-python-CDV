a16dacdd8df666f48d2992f615c62886da4b35c4
opopiol <57290198+opopiol@users.noreply.github.com>
2019-11-11 12:39:27 +0100

    Add files via upload

---
+++ 23.10zad6.py
@@ -0,0 +1,13 @@
+#Utwórz program, który będzie komunikować, czy wprowadzona liczba jest dodatnia czy nie
+
+print("Jest to program, który okresla czy liczba jest dodatnia") 
+print("wprowadz liczbe: ") 
+
+n=int(input())
+
+if(n>0):
+    print("Liczba jest dodatnia")
+    
+elif(n<=0):
+    print("Liczba nie jest dodatnia")
+    
